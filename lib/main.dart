20a00d95f1cbf31fe7957f5aa4a3c6ac1719fe59
joaoffnogueira <contact@joaonogueira.dev>
2022-03-11 09:57:11 -0300

    Add style

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:purple_store/pages/products_overview_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final ThemeData tema = ThemeData();
 
   // This widget is the root of your application.
   @override
@@ -14,8 +15,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.deepPurple,
+          secondary: Colors.deepPurpleAccent,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          headline6: const TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
       ),
       home: ProductsOverviewPage(),
     );
