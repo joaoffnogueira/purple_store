739c9518d915dc69f37ba3d15998759edb14fdb0
joaoffnogueira <contact@joaonogueira.dev>
2022-03-11 09:40:27 -0300

    Create products overview page

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:purple_store/data/dummy_data.dart';
+import 'package:purple_store/models/products.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  const ProductsOverviewPage({ Key? key }) : super(key: key);
+  List<Product> loadedProducts = dummyProducts;
+
+  ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Purple Store'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
+        ),
+      ),
     );
   }
 }
