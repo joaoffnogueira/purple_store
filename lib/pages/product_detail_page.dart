93c1a2e3854bc3c7a466d67f4e0098b71329d990
joaoffnogueira <contact@joaonogueira.dev>
2022-03-28 10:48:06 -0300

    Add product_detail_page content

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -14,6 +14,43 @@ class ProductDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title),
       ),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'R\$ ${product.price}',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Descrição',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            product.description,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
