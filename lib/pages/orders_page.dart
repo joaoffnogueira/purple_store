2aae583df3b7e0b36faf247f48a35079dbc8d840
joaoffnogueira <contact@joaonogueira.dev>
2022-04-01 10:10:22 -0300

    Change orders page to use FutureBuilder

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -4,43 +4,38 @@ import 'package:purple_store/components/app_drawer.dart';
 import 'package:purple_store/components/order_widget.dart';
 import 'package:purple_store/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({Key? key}) : super(key: key);
 
   @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(context, listen: false).loadOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of<OrderList>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading ? const Center(child: CircularProgressIndicator()) : ListView.builder(
-        itemCount: orders.ordersCount,
-        itemBuilder: (context, index) {
-          return OrderWidget(
-            order: orders.orders[index],
-          );
-        },
-      ),
+      body: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.error != null) {
+              return const Center(
+                child: Text('Ocorreu um erro ao carregar os pedidos'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (context, orders, child) => ListView.builder(
+                  itemCount: orders.ordersCount,
+                  itemBuilder: (context, index) {
+                    return OrderWidget(
+                      order: orders.orders[index],
+                    );
+                  },
+                ),
+              );
+            }
+          }),
     );
   }
 }
