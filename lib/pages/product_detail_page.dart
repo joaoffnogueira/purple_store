b92fa52d356ca13061b22ad807b3d4f9e3ec93db
joaoffnogueira <contact@joaonogueira.dev>
2022-04-06 16:53:06 -0300

    Add  SliverAppBar to product detail page

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -11,49 +11,66 @@ class ProductDetailPage extends StatelessWidget {
     final Product product =
         ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            height: 300,
-            width: double.infinity,
-            child: Hero(
-              tag: product.id,
-              child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: product.id,
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0, 0.8),
+                        end: Alignment(0, 0),
+                        colors: [
+                          Colors.black.withOpacity(0.6),
+                          Colors.black.withOpacity(0),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          const SizedBox(height: 10),
-          Text(
-            'R\$ ${product.price}',
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Text(
-            'Descrição',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            product.description,
-            style: TextStyle(
-              fontSize: 16,
-            ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 16),
+              Text(
+                'R\$ ${product.price.toStringAsFixed(2)}',
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                width: double.infinity,
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+              SizedBox(height: 16),
+            ]),
           ),
         ],
-      )),
+      ),
     );
   }
 }
